b05fbe4ca811af307b07af04d1b3b4ff9d53b9e5
robbinMayers <alex4emist@gmail.com>
2018-08-07 12:40:05 +0300

    Update MatchingEngine.java

--- MatchingEngine.java
+++ MatchingEngine.java
@@ -6,7 +6,12 @@ import java.lang.InterruptedException;
 public class MatchingEngine extends Thread {
 
     public static void main(String[] args) {
-
+            System.out.println("Commands:");
+            System.out.println("1. add / cancel - add or cancel an operation order");
+            System.out.println("2. somethingString - orders name");
+            System.out.println("3. S / B - sell or buy operation");
+            System.out.println("4. somethingInteger - price");
+            System.out.println("5. somethingInteger - quantity");
             new TradingGateway(new MatchingEngine());
 
     }
@@ -14,12 +19,12 @@ public class MatchingEngine extends Thread {
     public void run() {
 
             Trade trade = new Trade();
-            while (!Thread.currentThread().isInterrupted())
+            while (!isInterrupted())
             {
                 try
                 {
                     sleep(1000);
-                    trade.trade();
+                    trade.matching();
 
                 } catch(InterruptedException e) {
                     e.printStackTrace();
